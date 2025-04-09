8e48a9900fa5ea5642938060a5323d13fa9c24bc
Ivan Veltz <ivan.veltz@live.fr>
2025-04-09 11:37:43 +0200

    affichage post

--- forumPlateau_V2/controller/ForumController.php
+++ forumPlateau_V2/controller/ForumController.php
@@ -6,6 +6,7 @@ use App\AbstractController;
 use App\ControllerInterface;
 use Model\Managers\CategoryManager;
 use Model\Managers\TopicManager;
+use Model\Managers\PostManager;
 
 class ForumController extends AbstractController implements ControllerInterface{
 
@@ -41,5 +42,23 @@ class ForumController extends AbstractController implements ControllerInterface{
                 "topics" => $topics
             ]
         ];
+    }
+
+    public function listPostsByTopic($id){
+
+        $postManager = new PostManager();
+        $topicManager = new TopicManager();
+        $topic = $topicManager->findOneById($id);
+        $posts = $postManager->findPostsByTopic($id);
+
+        return [
+            "view" => VIEW_DIR."forum/listPosts.php",
+            "meta_description" => "Liste des posts par topics : ".$topic,
+            "data" => [
+                "topic" => $topic,
+                "posts" => $posts
+            ]
+        ];
+
     }
 }
---
+++ forumPlateau_V2/model/entities/Post.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Model\Entities;
+
+use App\Entity;
+
+final class Post extends Entity{
+
+    private $id;
+    private $text;
+    private $creationDate;
+    private $user;
+    private $topic;
+
+    public function __construct($data){
+        $this->hydrate($data); 
+    }
+
+    public function getId(){
+        return $this->id;
+    }
+
+    public function setId($id){
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getText(){
+        return $this->text;
+    }
+
+    public function setText($text){
+        $this->text = $text;
+        return $this;
+    }
+
+    public function getUser(){
+        return $this->user;
+    }
+
+    public function setUser($user){
+        $this->user = $user;
+        return $this;
+    }
+
+    public function getTopic(){
+        return $this->topic;
+    }
+
+    public function setTopic($topic){
+        $this->topic = $topic;
+        return $this;
+    }
+
+    public function __toString(){
+        return $this->text;
+    }
+}
